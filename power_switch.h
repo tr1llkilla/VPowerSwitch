b986d709338f6ec47c0e579888926a2414feac12
thaGnonsis <81764173+tr1llkilla@users.noreply.github.com>
2024-01-11 14:39:37 -0700

    Update power_switch.h

--- power_switch.h
+++ power_switch.h
@@ -1,3 +1,15 @@
+﻿/*
+Copyright (C) 2024 tr1llkilla
+    This program comes with ABSOLUTELY NO WARRANTY.
+    This is free software, and you are welcome to redistribute it
+    under legal conditions.
+Author:
+tr1llkilla
+
+Author's note:
+This program is unfinished, but does the trick to get the job done manually
+Feel free to credit this work in any future projects!
+*/
 #pragma once
 
 #include <httplib.h>
